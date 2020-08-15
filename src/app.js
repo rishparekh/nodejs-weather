e25e804537d8e79845ff3d06aea52f559eb23669
rishparekh@gmail.com <rishparekh@gmail.com>
2020-08-15 11:53:44 +0530

    set up for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geoCode = require('./utils/geoCode')
 const foreCast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -103,6 +104,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up")
+app.listen(port, () => {
+    console.log("Server is up " + port)
 })
